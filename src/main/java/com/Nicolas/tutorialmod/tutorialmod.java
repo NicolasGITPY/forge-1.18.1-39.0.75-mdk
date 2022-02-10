d3564afc79f6505cc112589964339d12f3581275
Nicolas <nicolas.herberich420@gmail.com>
2022-02-10 21:25:56 +0100

    added two items

--- src/main/java/com/Nicolas/tutorialmod/tutorialmod.java
+++ src/main/java/com/Nicolas/tutorialmod/tutorialmod.java
@@ -1,21 +1,14 @@
 package com.Nicolas.tutorialmod;
 
-import net.minecraft.world.level.block.Block;
+import com.Nicolas.tutorialmod.item.ModItems;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(tutorialmod.MOD_ID)
@@ -26,8 +19,11 @@ public class tutorialmod {
     private static final Logger LOGGER = LogManager.getLogger();
 
     public tutorialmod() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+        eventBus.addListener(this::setup);
 
 
         // Register ourselves for server and other game events we are interested in
