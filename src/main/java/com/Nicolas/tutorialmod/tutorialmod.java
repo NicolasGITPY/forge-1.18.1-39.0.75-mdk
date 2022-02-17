b0825b9e49a72f9f10d57be0745c269cff5c53be
Nicolas <nicolas.herberich420@gmail.com>
2022-02-17 17:26:44 +0100

    added a door, trapdoor, pressure plate and button

--- src/main/java/com/Nicolas/tutorialmod/tutorialmod.java
+++ src/main/java/com/Nicolas/tutorialmod/tutorialmod.java
@@ -2,10 +2,13 @@ package com.Nicolas.tutorialmod;
 
 import com.Nicolas.tutorialmod.block.ModBlocks;
 import com.Nicolas.tutorialmod.item.ModItems;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
@@ -26,10 +29,17 @@ public class tutorialmod {
         ModItems.register(eventBus);
         ModBlocks.register(eventBus);
         eventBus.addListener(this::setup);
+        eventBus.addListener(this::clientSetup);
 
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event){
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.EBONY_DOOR.get(), RenderType.translucent());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.EBONY_TRAPDOOR.get(), RenderType.translucent());
+
     }
 
     private void setup(final FMLCommonSetupEvent event) {
